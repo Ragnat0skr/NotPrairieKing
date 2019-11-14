4eaed9cac2cadab6e56c7d45927618ead517bca8
Ragnat0skr <43636939+Ragnat0skr@users.noreply.github.com>
2019-11-14 22:45:38 +0000

    Add files via upload

---
+++ bullet.py
@@ -0,0 +1,97 @@
+# Skeleton for new projects
+# CTRL+SHIFT+B to run
+import pygame
+import random
+
+WIDTH = 360
+HEIGHT = 460
+FPS = 30
+
+# Define colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+#Initialise and create window
+pygame.init()
+pygame.mixer.init() #Handles sound
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("MyGame") #title
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,40))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        # self.rect.x += self.speedx
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -10
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 10
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top) # Top of bullet top of player
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+    # Kill if off the top
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+#Game Loop
+running = True
+while running:
+    # Keep running at right speed
+    clock.tick(FPS)
+    # Process input (events)
+    for event in pygame.event.get():
+        #check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+    #Update
+    all_sprites.update()
+    #Draw/render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    #Flip after drawing everything
+    pygame.display.flip()
+
+pygame.quit()
